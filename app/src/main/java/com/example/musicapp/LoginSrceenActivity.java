16cd89d29081ed4b98f2d54f514e7ba1c2857563
pokiwer <longcon222001@gmai.com>
2023-10-19 21:56:13 +0700

    update login_ui

---
+++ app/src/main/java/com/example/musicapp/LoginSrceenActivity.java
@@ -0,0 +1,14 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginSrceenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_srceen);
+    }
+}
